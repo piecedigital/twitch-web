b30986a4c608cc89c1930f9b29bf0842ac2355f1
Darryl Dixon <piecedigitalstudios@gmail.com>
2016-03-09 02:14:48 -0500

    fixed missing module bug

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 var express = require("express"),
 	app = express(),
 	port = process.env["PORT"] || 8080,
-	morgan = require("morgan"),
 
 	path = require("path");
 process.env["NODE_ENV"] = process.env["NODE_ENV"] || "dev";
 
-app.use(morgan("dev"));
+if(process.env["NODE_ENV"] !== "dev") {
+	morgan = require("morgan");
+	app.use(morgan("dev"));
+};
 app.use(express.static(path.join(__dirname, "public")));
 app.use(require("./modules/routes"));
 
